b18c5e22aa6cdffd16f6ebb7e625fe2a3871018f
Ankur Gajurel <ankurgajurel02@gmail.com>
2024-01-02 01:17:16 +0545

    build fix

--- next.config.js
+++ next.config.js
@@ -1,28 +1,30 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
+// const withBundleAnalyzer = require("@next/bundle-analyzer")({
+//   enabled: process.env.ANALYZE === "true",
+// });
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withBundleAnalyzer({
-  reactStrictMode: true,
-  pageExtensions: ["ts", "tsx", "js"],
-  eslint: {
-    dirs: ["src"],
-  },
-  images: {
-    domains: ["https://flagcdn.com"],
-  },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-      net: false,
-      dns: false,
-      child_process: false,
-      tls: false,
-    };
+// const nextConfig = withBundleAnalyzer({
+//   reactStrictMode: true,
+//   pageExtensions: ["ts", "tsx", "js"],
+//   eslint: {
+//     dirs: ["src"],
+//   },
+//   images: {
+//     domains: ["https://flagcdn.com"],
+//   },
+//   webpack: (config) => {
+//     config.resolve.fallback = {
+//       fs: false,
+//       net: false,
+//       dns: false,
+//       child_process: false,
+//       tls: false,
+//     };
 
-    return config;
-  },
-});
+//     return config;
+//   },
+// });
+
+const nextConfig = {}
 
 module.exports = nextConfig;
--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  bracketSpacing: true,
-  semi: true,
-  singleQuote: false,
-  trailingComma: "all",
-  printWidth: 80,
-  tabWidth: 2,
-  plugins: [require("prettier-plugin-tailwindcss")],
-};
